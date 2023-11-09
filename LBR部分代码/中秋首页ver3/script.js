1d422d94dd708ebb62723cf34f41b6476465b2cf
meti-liu <2397169271@qq.com>
2023-11-09 14:25:13 +0800

    change

--- LBR部分代码/中秋首页ver3/script.js
+++ LBR部分代码/中秋首页ver3/script.js
@@ -6,8 +6,8 @@ let right2 = document.getElementById('right2');
 
 window.addEventListener('scroll', function () {
   let value = window.scrollY;
-  if (value < 1000) { // ·ÀÖ¹ÎÞÏÞ»®ÆÁÄ»
-    moon.style.marginTop = value * 1 + 'px';
+  if (value < 1000) { 
+    moon.style.marginTop = value * 0.5+ 'px';
     left1.style.left = value * -3.5 + 'px';
     left2.style.left = value * -3.5 + 'px';
     right1.style.left = value * 3.5 + 'px';
