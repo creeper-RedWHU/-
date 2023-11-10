f25f4e70cc0a3595386a7df3b984807d28f84946
ZJY <hello@world.com>
2023-11-10 09:29:53 +0800

    赏月部分

--- ZJY部分代码/static/js/赏月.js
+++ ZJY部分代码/static/js/赏月.js
@@ -1,22 +1,27 @@
-var img_music = document.getElementById("img")
-var audio = document.getElementById("audio")
-var test=1/*test = 0，起始状态：播放状态；test=1：暂停状态*/
-img_music.onclick = function (){
-    if(test === 1){
-        img_music.src = "static/img/music-off.png"
-        audio.pause()
-    }else{
-        img_music.src = "static/img/music.png"
-        audio.play()
-    }
-    test = 1-test
+const toCnDate = date => date.toLocaleString('zh-u-ca-chinese', { dateStyle: 'full' }) + ' ' + date.toLocaleTimeString(0, { hour12: false });
+var moon_phase = document.getElementById("img")
+var calendar = document.getElementById("calendar")
+var important_icon = document.getElementById("important")
+var time_stamp = new Date()
+var year = time_stamp.getFullYear(); //获取完整的年份(4位)
+var month =time_stamp.getMonth()+1;  //获取当前月份(1-12,1代表1月)
+var day =time_stamp.getDate();  //获取当前日(1-31)
+var time_area = document.getElementById("time")
+var lunarDate = toCnDate(new Date()).slice(4,14)
+var month_str = (month<10)?'0'+ month.toString(): month.toString()
+var day_str = (month<10)?'0'+day.toString():day.toString()
+var week = time_stamp.getDay();   //获取当前星期X(0-6,0代表星期天)
+var weekList = ['天','一','二','三','四','五','六']
+var dateList = [year.toString()+'.'+month_str+'.'+day_str+' 星期'+weekList[week],lunarDate]
+var ptr = 0
+window.onload = function (){
+    time_area.innerHTML =dateList[ptr]
 }
-const app =new Vue({
-    el:"#right",
-
-})
-
-
-
-
-
+time_area.onclick = function (){
+    if(ptr<dateList.length-1){
+        ptr+=1
+    }else{
+        ptr = 0
+    }
+    time_area.innerHTML = dateList[ptr]
+}
