3f65f751bdcc8d8b0dc15ac91eaa9d0746c5f2c3
meti-liu <2397169271@qq.com>
2023-11-10 20:51:22 +0800

    1

--- LBR部分代码/中秋首页ver3/script.js
+++ LBR部分代码/中秋首页ver3/script.js
@@ -4,15 +4,22 @@ let left2 = document.getElementById('left2');
 let right1 = document.getElementById('right1');
 let right2 = document.getElementById('right2');
 
-window.addEventListener('scroll', function () {
+window.onload = function() 
+{
+  window.scrollTo(0, 0);
+}
+
+window.addEventListener('scroll', function () 
+{
   let value = window.scrollY;
-  if (value < 1000) { 
-    moon.style.marginTop = value * 0.5+ 'px';
+  if (value < 1000) 
+  { 
+    moon.style.marginTop = value * -2+ 'px';
     left1.style.left = value * -3.5 + 'px';
     left2.style.left = value * -3.5 + 'px';
     right1.style.left = value * 3.5 + 'px';
     right2.style.left = value * 4 + 'px';
   }
-
+  
 });
 
