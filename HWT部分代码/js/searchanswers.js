319f5b64d0cd218b8737fca5a235b8654187817f
wurldwen <1287356875@qq.com>
2023-11-17 15:44:49 +0800

    bug修复，代码完善与注释

--- HWT部分代码/js/searchanswers.js
+++ HWT部分代码/js/searchanswers.js
@@ -1,3 +1,4 @@
+//将用户可以看到的灯谜创建成库
  var data = [
             { id: 1, title: "水中捞月（打一成语）", content: "谜底：有影无踪/求之不得/浮光掠影..." },
             { id: 2, title: "十五的月亮（打一成语）", content: "谜底：正大光明<br>解析：十五的月亮正是又大又明亮的时候，该成语形容心怀坦白，言行正派。" },
@@ -5,12 +6,10 @@
             { id: 4, title: "一撇划了三寸长（打一字）", content: "谜底：寿<br> 解析:‘一撇’会意代扣‘丿’,丿+三+寸=寿，所以答案是寿" },
             { id: 5, title: "丘山认父（打一字）", content: "谜底：岳<br>解析：丘+山=岳。谜面中的“父”理解为岳父，扣“岳”，第二次紧扣谜底。" },
         ];
-
         // 获取搜索输入框
         var sInput = document.getElementById('Input');
         // 获取搜索结果容器
         var sResults = document.getElementById('Results');
-
         // 监听输入框的键盘按下事件
         sInput.addEventListener('input', function() {
             // 执行搜索
@@ -23,22 +22,22 @@
                 enterFunction(this.value.toLowerCase());
             }
         });
-
+        //即是响应搜索函数实现
         function followFunction(input) {
-            // 进行搜索
+            // 防止输入结果为空时所有答案均显示或输入一时出现两个答案
             if (input.trim() !== ""&&input.trim()!="一") {  
                 var results = data.filter(item => item.title.toLowerCase().includes(input));
                 if (results.length > 0) {
                     showSResults(results);
                 }
             } else {
-                sResults.innerHTML = "";  // 清空搜索结果
+                sResults.innerHTML = "";  
             }
         }
-
+        //按下回车键后再搜索函数实现
         function enterFunction(input) {
             // 进行搜索
-            if (input.trim() !== "") {  // 检查输入是否为空
+            if (input.trim() !== "") { 
                 var results = data.filter(item => item.title.toLowerCase().includes(input));
                 if (results.length > 0) {
                     showSResults(results);
@@ -46,14 +45,11 @@
                     failFound();
                 }
             } else {
-                sResults.innerHTML = "";  // 清空搜索结果
+                sResults.innerHTML = ""; 
             }
         }
         function showSResults(results) {
-            // 清空之前的搜索结果
             sResults.innerHTML = "";
-
-            // 显示搜索结果
             for (var i = 0; i < results.length; i++) {
                 var result = results[i];
                 var resultItem = document.createElement('div');
@@ -61,16 +57,14 @@
                 sResults.appendChild(resultItem);
             }
         }
-
+        //查询失败函数实现
         function failFound() {
-            // 清空之前的搜索结果
             sResults.innerHTML = "";
-
-            // 显示未找到结果的消息
             var Message = document.createElement('div');
             Message.textContent = "抱歉，没有找到您想要的内容。";
             sResults.appendChild(Message);
         }
+        
         
 
    
